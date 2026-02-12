842b09332099adadd3377fe699d0956f956dd379
TheCoolness8376 <TheCoolness8376@gmail.com>
2026-02-12 09:56:42 -0500

    Save

---
+++ TicTacToe.py
@@ -0,0 +1,23 @@
+# start with a match to select to play with another player to a cpu
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# ---------- WHATS NEEDED ---------- #
+# Put a X or O with column and row
+#
+#
+#
+#
+#
+#
+#
+#
+#
