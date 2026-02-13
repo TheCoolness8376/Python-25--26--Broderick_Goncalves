b45ad3bef2a02f5d3acd37ea9cfa5dc4eff07ace
TheCoolness8376 <TheCoolness8376@gmail.com>
2026-02-13 08:03:50 -0500

    Challenge Done

---
+++ CodingChallengeTwo_BGoncalves.py
@@ -0,0 +1,14 @@
+# Challenge 2 Starter Code
+user_input = input("Enter a word: ")
+if len(user_input) > 5:
+    print("Your word is a Long Word")
+else:
+    print("Your word is a Short Word")
+print("Your word is: " + user_input.lower())
+print("Your word is: " + user_input.upper())
+print("The length of your word is: " + len(user_input))
+
+# ---------- Q & A ---------- #
+# 1. len() returns the length of the word thats inputted
+# 2. Input must be cast first before math operations to get the users number input
+# 3. == checks for equality
