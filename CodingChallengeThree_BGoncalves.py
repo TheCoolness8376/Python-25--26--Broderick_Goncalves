4671094441c06f94f814b0536f9003d3964f9a12
TheCoolness8376 <TheCoolness8376@gmail.com>
2026-02-18 09:55:20 -0500

    Temp Converter

---
+++ CodingChallengeThree_BGoncalves.py
@@ -0,0 +1,27 @@
+# ---------- Practice Activity 1 ---------- #
+import sys
+while True:
+    Annoy = input("Are you annoyed? (You can quit at any time using quit) ")
+    if input == "quit":
+        print("quitting")
+        sys.exit(0)
+        break
+    Annoy2 = input("Are you annoyed now? ")
+    if input == "quit":
+        print("quitting")
+        sys.exit(0)
+        break
+    Annoy3 = input("What about now? ")
+    if input == "quit":
+        print("quitting")
+        sys.exit(0)
+        break
+    Annoy4 = input("Ok, now you're annoyed. ")
+    if input == "quit":
+        print("quitting")
+        sys.exit(0)
+        break
+
+# ---------- Practice Activity 2 ---------- #
+for i in range(1, 11):
+    print(i)
